e7190ed1d83eb18b35ffa3ae21ddd38b89713e23
Silvia Witzig <silvia.witzig@unibas.ch>
2024-03-01 17:03:21 +0100

    add fields

--- pkg/schema/ubschema001.go
+++ pkg/schema/ubschema001.go
@@ -48,11 +48,12 @@ type OriginInfo struct {
 	Date      string   `json:"date,omitempty"`
 }
 
-type AccessCondition struct {
-	Add     []string `json:"add,omitempty"`
-	Licence []string `json:"licence,omitempty"`
-	Main    []string `json:"main,omitempty"`
-	Url     []string `json:"url,omitempty"`
+type NoteWithURL struct {
+	Add     string `json:"add,omitempty"`
+	Licence string `json:"licence,omitempty"`
+	Main    string `json:"main,omitempty"`
+	Url     string `json:"url,omitempty"`
+	//Url     []string `json:"url,omitempty"`
 }
 
 type Cartographics struct {
@@ -62,19 +63,75 @@ type Cartographics struct {
 
 type Coded struct {
 	Coordinates []string `json:"coordinates,omitempty"`
-	Scale       []string `json:"scale,omitempty"`
+	Scale       string   `json:"scale,omitempty"`
 }
 
 type Display struct {
-	Coordinates []string `json:"coordinates,omitempty"`
-	Other       []string `json:"other,omitempty"`
-	Projection  []string `json:"projection,omitempty"`
-	Scale       []string `json:"scale,omitempty"`
+	Coordinates string `json:"coordinates,omitempty"`
+	Other       string `json:"other,omitempty"`
+	Projection  string `json:"projection,omitempty"`
+	Scale       string `json:"scale,omitempty"`
 }
 
+type Extension struct {
+	LocalCodeIZ        []string `json:"localCodeIZ,omitempty"`
+	LocalCodeNZ        []string `json:"localCodeNZ,omitempty"`
+	SubjectCategoryUbs []string `json:"subjectCategoryUbs,omitempty"`
+}
+
+type Identifier struct {
+	Doi                      []string   `json:"doi,omitempty"`
+	Isbn                     []IsbnIsmn `json:"isbn,omitempty"`
+	IsbnInvalid              []string   `json:"isbnInvalid,omitempty"`
+	Ismn                     []IsbnIsmn `json:"ismn,omitempty"`
+	Issn                     []string   `json:"issn,omitempty"`
+	IssnInvalid              []string   `json:"issnInvalid,omitempty"`
+	Issnl                    []string   `json:"issnl,omitempty"`
+	IssueNumber              []string   `json:"issueNumber,omitempty"`
+	MatrixNumber             []string   `json:"matrixNumber,omitempty"`
+	MusicPlate               []string   `json:"musicPlate,omitempty"`
+	MusicPublisher           []string   `json:"musicPublisher,omitempty"`
+	Urn                      []string   `json:"urn,omitempty"`
+	VideoRecordingIdentifier []string   `json:"videoRecordingIdentifier,omitempty"`
+}
+
+type IsbnIsmn struct {
+	Id        string   `json:"identifier,omitempty"`
+	Qualifier []string `json:"qualifier,omitempty"`
+}
+
+type Note struct {
+	Acquisition                     []string      `json:"acquisition,omitempty"`
+	Action                          []string      `json:"action,omitempty"`
+	AdditionalPhysicalForm          []NoteWithURL `json:"additionalPhysicalForm,omitempty"`
+	Bibliography                    []string      `json:"bibliography,omitempty"`
+	Binding                         []NoteWithURL `json:"binding,omitempty"`
+	Bibliographical                 []NoteWithURL `json:"bibliographical,omitempty"`
+	Citation                        []NoteWithURL `json:"citation,omitempty"`
+	Credits                         []string      `json:"credits,omitempty"`
+	Exhibitions                     []string      `json:"exhibitions,omitempty"`
+	Funding                         []string      `json:"funding,omitempty"`
+	General                         []string      `json:"general,omitempty"`
+	Han                             []string      `json:"han,omitempty"`
+	Language                        []string      `json:"language,omitempty"`
+	MediumOfPerformance             []string      `json:"mediumOfPerformance,omitempty"`
+	Other                           []string      `json:"other,omitempty"`
+	Ownership                       []NoteWithURL `json:"ownership,omitempty"`
+	Performers                      []string      `json:"performers,omitempty"`
+	PreferredCitation               []string      `json:"preferredCitation,omitempty"`
+	Publications                    []string      `json:"publications,omitempty"`
+	Reproduction                    []string      `json:"reproduction,omitempty"`
+	Restriction                     []NoteWithURL `json:"restriction,omitempty"`
+	StatementOfResponsibility       []string      `json:"statementOfResponsibility,omitempty"`
+	StatementOfResponsibilityAltRep []string      `json:"statementOfResponsibilityAltRep,omitempty"`
+	SystemDetails                   []string      `json:"systemDetails,omitempty"`
+	Thesis                          []string      `json:"thesis,omitempty"`
+	Venue                           []string      `json:"venue,omitempty"`
+	VersionIdentification           []string      `json:"versionIdentification,omitempty"`
+}
 type Mapping001 struct {
 	Abstract        []string                  `json:"abstract,omitempty"`
-	AccessCondition []AccessCondition         `json:"accessCondition,omitempty"`
+	AccessCondition []NoteWithURL             `json:"accessCondition,omitempty"`
 	Cartographics   Cartographics             `json:"cartographics,omitempty"`
 	Classification  map[string]Classification `json:"classification,omitempty"`
 	Date            []DateRange               `json:"date,omitempty"`
@@ -86,14 +143,14 @@ type Mapping001 struct {
 	Language            []string                   `json:"language,omitempty"`
 	Location            Location                   `json:"location,omitempty"`
 	Name                map[string]json.RawMessage `json:"name,omitempty"`
-	Note                map[string]json.RawMessage `json:"note,omitempty"`
+	Note                Note                       `json:"note,omitempty"`
 	OriginInfo          OriginInfo                 `json:"originInfo,omitempty"`
 	PhysicalDescription map[string]json.RawMessage `json:"physicalDescription,omitempty"`
 	RecordIdentifier    []string                   `json:"recordIdentifier,omitempty"`
 	RelatedItem         map[string]json.RawMessage `json:"relatedItem,omitempty"`
 	Subject             Subject                    `json:"subject,omitempty"`
-	TableOfContents     map[string]json.RawMessage `json:"tableOfContents,omitempty"`
-	TargetAudience      map[string]json.RawMessage `json:"targetAudience,omitempty"`
+	TableOfContents     []string                   `json:"tableOfContents,omitempty"`
+	TargetAudience      []string                   `json:"targetAudience,omitempty"`
 	TitleInfo           map[string]json.RawMessage `json:"titleInfo,omitempty"`
 }
 
