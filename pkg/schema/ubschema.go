59bf1e55f207de39cf9a72cf3cb12b3ec85f9e23
Juergen Enge <you@example.com>
2024-03-30 12:50:53 +0100

    schema funcs added

--- pkg/schema/ubschema.go
+++ pkg/schema/ubschema.go
@@ -1,3 +1,11 @@
 package schema
 
 type UBSchema UBSchema001
+
+func (u *UBSchema) GetPersons() map[string][]ResultPerson {
+	return (*UBSchema001)(u).GetPersons()
+}
+
+func (u *UBSchema) GetTitle() string {
+	return (*UBSchema001)(u).GetTitle()
+}
