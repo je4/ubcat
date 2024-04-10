46db30073e730b6ff34b59dc45c9668dec033512
Juergen Enge <you@example.com>
2024-04-10 13:09:46 +0200

    new result structure of search

--- pkg/index/client.go
+++ pkg/index/client.go
@@ -11,6 +11,13 @@ import (
 	"github.com/je4/ubcat/v2/pkg/schema"
 )
 
+type Result struct {
+	Docs  map[string]*schema.UBSchema
+	Total int64
+	From  int64
+	Num   int64
+}
+
 func NewClient(index string, client *elasticsearch.TypedClient) *Client {
 	return &Client{
 		index:  index,
@@ -70,7 +77,7 @@ func vectorQuery(vector []float32, field string) (*types.Query, error) {
 	}, nil
 }
 
-func (c *Client) Search(ctx context.Context, querystring string, vectorMarc, vectorJson, vectorProse []float32, from, num int) (map[string]*schema.UBSchema, error) {
+func (c *Client) Search(ctx context.Context, querystring string, vectorMarc, vectorJson, vectorProse []float32, from, num int64) (*Result, error) {
 	esMust := []types.Query{}
 	if len(vectorMarc) > 0 {
 		vQuery, err := vectorQuery(vectorMarc, "embedding_marc")
@@ -112,22 +119,30 @@ func (c *Client) Search(ctx context.Context, querystring string, vectorMarc, vec
 		Index(c.index).
 		// SourceExcludes_("embedding_marc", "embedding_json", "embedding_prose").
 		Request(searchRequest).
-		From(from).
-		Size(num)
+		From(int(from)).
+		Size(int(num))
 
 	elasticResponse, err := elasticQuery.Do(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot search")
 	}
 
-	var result = map[string]*schema.UBSchema{}
+	var docs = map[string]*schema.UBSchema{}
 	for _, hit := range elasticResponse.Hits.Hits {
 		var s = &schema.UBSchema{}
 		if err := json.Unmarshal(hit.Source_, s); err != nil {
 			return nil, errors.Wrapf(err, "cannot unmarshal document %v", hit.Source_)
 		}
 		s.Score_ = float64(hit.Score_)
-		result[hit.Id_] = s
+		docs[hit.Id_] = s
+	}
+	result := &Result{
+		Docs: docs,
+		From: from,
+		Num:  num,
+	}
+	if elasticResponse.Hits.Total != nil {
+		result.Total = elasticResponse.Hits.Total.Value
 	}
 	return result, nil
 }
