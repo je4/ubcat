004e01472f7bd13c74682eb3f9c928afc36e8031
Juergen Enge <you@example.com>
2024-09-11 12:05:13 +0200

    initial mapping for rdv

---
+++ pkg/mappingRDV/mapping.go
@@ -0,0 +1,120 @@
+package mappingRDV
+
+import (
+	"fmt"
+	"github.com/je4/ubcat/v2/pkg/schema"
+	"strings"
+)
+
+type MappingRDV schema.UBSchema001
+
+type Element struct {
+	Text string `json:"text"`
+	Link string `json:"link"`
+}
+
+func (e Element) String() string {
+	if e.Link != "" {
+		return fmt.Sprintf("'%s:[%s]'", e.Text, e.Link)
+	}
+	return fmt.Sprintf("'%s'", e.Text)
+}
+
+func (m *MappingRDV) GetOriginInfoProduction() (key string, result []Element, ok bool) {
+	if m.Mapping == nil {
+		return
+	}
+	if m.Mapping.OriginInfo == nil {
+		return
+	}
+	if len(m.Mapping.OriginInfo.Production) == 0 {
+		return
+	}
+	result = []Element{}
+	key = "originInfo"
+	ok = true
+	for _, v := range m.Mapping.OriginInfo.Production {
+		if v == nil {
+			continue
+		}
+		e := Element{}
+		e.Text = strings.TrimRight(strings.Join([]string{
+			strings.Join(v.Place, ", "),
+			strings.Join(v.Publisher, ", "),
+			v.Date}, "; "), " ;")
+		result = append(result, e)
+	}
+	return
+}
+
+func (m *MappingRDV) GetMainTitle() (key string, result []Element, ok bool) {
+	if m.Mapping == nil {
+		return
+	}
+	if m.Mapping.TitleInfo == nil {
+		return
+	}
+	if len(m.Mapping.TitleInfo.Main) == 0 {
+		return
+	}
+	result = []Element{}
+	key = "mainTitle"
+	ok = true
+	for _, v := range m.Mapping.TitleInfo.Main {
+		if v == nil {
+			continue
+		}
+		e := Element{
+			Text: v.Title,
+		}
+		result = append(result, e)
+	}
+	return
+}
+
+func (m *MappingRDV) GetFacetAutographScribe() (key string, result []Element, ok bool) {
+	key = "facetAutographScribe"
+	ok = true
+	result = []Element{}
+	for _, f := range m.Facets {
+		for _, af := range f.Agents {
+			if af.Name != "scribe" {
+				continue
+			}
+			for _, a := range af.Agent {
+				e := Element{
+					Text: a.Label,
+				}
+				if len(a.Identifer) > 0 {
+					e.Link = fmt.Sprintf("facet:scribe:%s", a.Identifer[0])
+				}
+				result = append(result, e)
+			}
+		}
+	}
+	if len(result) == 0 {
+		return "", nil, false
+	}
+	return
+}
+
+func (m *MappingRDV) Map() (result map[string][]Element) {
+	result = map[string][]Element{}
+	if m.Mapping == nil {
+		return
+	}
+	key, value, ok := m.GetMainTitle()
+	if ok {
+		result[key] = value
+	}
+	key, value, ok = m.GetOriginInfoProduction()
+	if ok {
+		result[key] = value
+	}
+
+	key, value, ok = m.GetFacetAutographScribe()
+	if ok {
+		result[key] = value
+	}
+	return
+}
---
+++ pkg/mappingRDV/mapping_test.go
@@ -0,0 +1,131 @@
+package mappingRDV
+
+import (
+	"github.com/je4/ubcat/v2/pkg/schema"
+	"testing"
+	"time"
+)
+
+func TestMappingRDV(t *testing.T) {
+	content := &schema.UBSchema001{
+		Timestamp:    time.Time{},
+		OaiId:        nil,
+		Ldr:          nil,
+		Controlfield: nil,
+		Mapping: &schema.Mapping001{
+			Abstract:        nil,
+			AccessCondition: nil,
+			Cartographics:   nil,
+			Classification:  nil,
+			Date:            nil,
+			Extension:       nil,
+			Files:           nil,
+			Fulltext:        nil,
+			Identifier:      nil,
+			Language:        nil,
+			Location:        nil,
+			Name:            nil,
+			Note:            nil,
+			OriginInfo: &schema.OriginInfo{
+				CopyrightDate: nil,
+				Distribution:  nil,
+				Edition:       nil,
+				EditionAltRep: nil,
+				Geographic:    nil,
+				Manufacture:   nil,
+				Production: []*schema.PublicationNote{
+					{
+						Date:        "",
+						LinkedField: "",
+						Place:       []string{"Place 1", "Place 2"},
+						Publisher:   []string{"Publisher 1", "Publisher 2"},
+					},
+				},
+				Publication: nil,
+			},
+			PhysicalDescription: nil,
+			RecordIdentifier:    nil,
+			RelatedItem:         nil,
+			Subject:             nil,
+			TableOfContents:     nil,
+			TargetAudience:      nil,
+			TitleInfo: &schema.TitleInfo{
+				Abbreviated: nil,
+				Alternative: nil,
+				Main: []*schema.Title{
+					{
+						Title:       "Title 1",
+						SubTitle:    "",
+						PartName:    nil,
+						PartNumber:  nil,
+						LinkedField: "",
+					},
+					{
+						Title:       "Title 2",
+						SubTitle:    "",
+						PartName:    nil,
+						PartNumber:  nil,
+						LinkedField: "",
+					},
+				},
+				Translated: nil,
+				Uniform:    nil,
+			},
+		},
+		Facets: []schema.Facets{
+			{
+				Agents: []schema.AgentFacets{
+					{
+						Name: "scribe",
+						Agent: []schema.Agent{
+							{
+								Identifer: []string{"4711"},
+								Label:     "AgentLabel 1",
+								Role:      nil,
+							},
+						},
+					},
+				},
+				Concepts:  nil,
+				Daterange: nil,
+				Strings:   nil,
+			},
+		},
+		Sets:           nil,
+		Flags:          nil,
+		ACL:            schema.ACL{},
+		EmbeddingProse: nil,
+		EmbeddingMarc:  nil,
+		EmbeddingJson:  nil,
+	}
+
+	result := (*MappingRDV)(content).Map()
+	t.Logf("result: %v", result)
+	/*
+		if fmt.Sprintf("%v", result) != "map[mainTitle:[{Title 1  } {Title 2  }] originInfo:[{Place 1, Place 2  }]]" {
+			t.Errorf("unexpected result: %v", result)
+		}
+	*/
+	if titles, ok := result["mainTitle"]; !ok {
+		t.Errorf("mainTitle not found")
+	} else {
+		if len(titles) != 2 {
+			t.Errorf("expected 2 titles, got %d", len(titles))
+		}
+	}
+	if originInfos, ok := result["originInfo"]; !ok {
+		t.Errorf("originInfo not found")
+	} else {
+		if len(originInfos) != 1 {
+			t.Errorf("expected 1 originInfo, got %d", len(originInfos))
+		}
+	}
+	if facetAutographScribe, ok := result["facetAutographScribe"]; !ok {
+		t.Errorf("facetAutographScribe not found")
+	} else {
+		if len(facetAutographScribe) != 1 {
+			t.Errorf("expected 1 facetAutographScribe, got %d", len(facetAutographScribe))
+		}
+	}
+
+}
